ac23084b95a6500e981fb6694ca67ef2d8239c00
daisy1754 <d-part@hotmail.co.jp>
2013-08-02 16:53:49 +0900

    initial commit; auto-generated project boilerplate

---
+++ src/jp/gr/java_conf/daisy/android_blocks/MainActivity.java
@@ -0,0 +1,16 @@
+package jp.gr.java_conf.daisy.android_blocks;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Main activity  that links to activity to demonstrate my building blocks.
+ */
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
